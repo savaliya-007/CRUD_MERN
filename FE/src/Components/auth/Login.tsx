8e08dc970d3163284392ab86e1922d24d3080108
Sun-007 <138206213+Umesh-WW@users.noreply.github.com>
2024-01-12 18:30:26 +0530

    done form for sign up and login

--- FE/src/Components/auth/Login.tsx
+++ FE/src/Components/auth/Login.tsx
@@ -1,33 +1,75 @@
-import { Box, Container, FormControl, FormHelperText, Input, InputLabel, Paper } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  Paper,
+} from "@mui/material";
 import "./auth.css";
-import { Formik, useFormik } from "formik";
+import { useFormik } from "formik";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import axiosClient from "../../lib/axiosClient";
+// import { useDispatch } from "react-redux";
 
 // -----------------------------------------
 
 export default function Login() {
+  // const dispatch = useDispatch();
+
   const formik = useFormik({
-    initialValues:{ username: "", password: "" },
-    onSubmit: ()=>{} 
-  })
-  console.log(formik.values.username)
+    initialValues: { username: "", password: "", visibility: false },
+    validate: (value) => {
+      const error: { username?: string; password?: string } = {};
+      if (value.password.trim().length < 4) {
+        error.password = "password must be at least 4 caracters long";
+      }
+      if (value.username.trim().length < 4) {
+        error.username = "username must be at least 4 caracters long";
+      }
+
+      return error;
+    },
+    onSubmit: (value) => {
+      console.log("value==============================", value);
+      axiosClient.post(
+        "/api/v1/user/login",
+        { username: value.username, password: value.password },
+        { headers: { "Content-Type": "application/json" } }
+      ).then(response => {
+        console.log('response', response)
+        //dispatch
+      });
+    },
+  });
+
   return (
     <Container maxWidth="lg">
       <Paper className="main-page">
-        <Box component={"h1"} display={"flex"} justifyContent={"center"}>
-          LOGIN
-        </Box>
+        <Box component={"h1"}> LOGIN</Box>
         <Box
           component={"form"}
           className="form-login"
           display={"flex"}
           justifyContent={"center"}
+          flexDirection={"column"}
           sx={{
-            display: "grid",
-            margin: "20px",
-            placeItems: "center",
+            paddingTop: "6%",
+            paddingLeft: "10%",
+            paddingRight: "10%",
+            gap: "40px",
+            maxWidth: "400px",
+            width: "100%",
           }}
+          onSubmit={formik.handleSubmit}
         >
-          <FormControl  fullWidth>
+          <FormControl
+            fullWidth
+            error={formik.touched.username && formik.errors?.username != void 0}
+          >
             <InputLabel
               variant="standard"
               disableAnimation
@@ -44,7 +86,10 @@ export default function Login() {
               id="username"
             />
           </FormControl>
-          <FormControl  fullWidth>
+          <FormControl
+            fullWidth
+            error={formik.touched.password && formik.errors.password !== void 0}
+          >
             <InputLabel
               variant="standard"
               disableAnimation
@@ -59,8 +104,32 @@ export default function Login() {
               value={formik.values.password}
               name="password"
               id="password"
+              type={formik.values.visibility ? "password" : "text"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() =>
+                      formik.setValues({
+                        ...formik.values,
+                        visibility: !formik.values.visibility,
+                      })
+                    }
+                    name="visibility"
+                  >
+                    {formik.values.visibility ? (
+                      <Visibility />
+                    ) : (
+                      <VisibilityOff />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
             />
           </FormControl>
+          <Button fullWidth variant="outlined" type="submit">
+            Login
+          </Button>
         </Box>
       </Paper>
     </Container>
--- FE/src/Components/auth/SignUp.tsx
+++ FE/src/Components/auth/SignUp.tsx
@@ -1,3 +1,182 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  Paper,
+} from "@mui/material";
+import "./auth.css";
+import { useFormik } from "formik";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import axiosClient from "../../lib/axiosClient";
+// import { useDispatch } from "react-redux";
+
+// -----------------------------------------
+
 export default function SignUp() {
-  return <>Hii SignIo</>;
+  const formik = useFormik({
+    initialValues: { username: "", email: "", password: "", visibility: false , isAdmin: false},
+    validate: (value) => {
+      const error: { username?: string; password?: string; email?: string } =
+        {};
+      if (value.password.trim().length < 4) {
+        error.password = "password must be at least 4 caracters long";
+      }
+      if (value.username.trim().length < 4) {
+        error.username = "username must be at least 4 caracters long";
+      }
+
+      if (value.email.trim().length < 4) {
+        error.email = "email must be at least 4 caracters long";
+      }
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      console.log("emailRegex.test(value.email)", emailRegex.test(value.email));
+      if (!emailRegex.test(value.email)) {
+        error.email = "invalid email!";
+      }
+
+      return error;
+    },
+    onSubmit: (value) => {
+      console.log("value==============================", value);
+      axiosClient
+        .post(
+          "/api/v1/user/register",
+          {
+            username: value.username,
+            password: value.password,
+            email: value.email,
+          },
+          { headers: { "Content-Type": "application/json" } }
+        )
+        .then((response) => {
+          console.log("response", response);
+          //dispatch
+        });
+    },
+  });
+
+  return (
+    <Container maxWidth="lg">
+      <Paper className="main-page signup">
+        <Box component={"h1"}> SIGN UP</Box>
+        <Box
+          component={"form"}
+          className="form-login"
+          display={"flex"}
+          justifyContent={"center"}
+          flexDirection={"column"}
+          sx={{
+            paddingTop: "6%",
+            paddingLeft: "10%",
+            paddingRight: "10%",
+            gap: "40px",
+            maxWidth: "400px",
+            width: "100%",
+          }}
+          onSubmit={formik.handleSubmit}
+        >
+          <FormControl
+            fullWidth
+            error={formik.touched.username && formik.errors?.username != void 0}
+          >
+            <InputLabel
+              variant="standard"
+              disableAnimation
+              shrink
+              size="normal"
+              htmlFor="username"
+            >
+              username
+            </InputLabel>
+            <Input
+              onChange={formik.handleChange}
+              value={formik.values.username}
+              name="username"
+              id="username"
+            />
+          </FormControl>
+          <FormControl
+            fullWidth
+            error={formik.touched.email && formik.errors.email !== void 0}
+          >
+            <InputLabel
+              variant="standard"
+              disableAnimation
+              shrink
+              size="normal"
+              htmlFor="email"
+            >
+              Email
+            </InputLabel>
+            <Input
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              name="email"
+              id="email"
+            />
+          </FormControl>
+
+          <FormControl
+            fullWidth
+            error={formik.touched.password && formik.errors.password !== void 0}
+          >
+            <InputLabel
+              variant="standard"
+              disableAnimation
+              shrink
+              size="normal"
+              htmlFor="password"
+            >
+              password
+            </InputLabel>
+            <Input
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              name="password"
+              id="password"
+              type={formik.values.visibility ? "password" : "text"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() =>
+                      formik.setValues({
+                        ...formik.values,
+                        visibility: !formik.values.visibility,
+                      })
+                    }
+                    name="visibility"
+                  >
+                    {formik.values.visibility ? (
+                      <Visibility />
+                    ) : (
+                      <VisibilityOff />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+
+          <FormControlLabel
+            value="end"
+            control={<Checkbox value={formik.values.isAdmin} />}
+            label="sign up as Administrator"
+            labelPlacement="end"
+          />
+          <Button fullWidth variant="outlined" type="submit">
+            Sign Up
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
