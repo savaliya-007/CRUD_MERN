87b43a8ad037185ffd5ef9f529ba758094fbbfb6
Sun-007 <138206213+Umesh-WW@users.noreply.github.com>
2024-01-24 17:15:33 +0530

    try to config eslint

--- BE/src/model/UserModel.ts
+++ BE/src/model/UserModel.ts
@@ -1,2 +1,17 @@
-import mon
-const UserScema = 
+import mongoose, { Document } from "mongoose";
+
+export interface UserType extends Document {
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+const UserScema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+})  
+
+export default mongoose.model<UserType>("User", UserScema)
